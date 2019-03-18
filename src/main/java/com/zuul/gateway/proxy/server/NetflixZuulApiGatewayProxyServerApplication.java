1d81f85a2233c58e5c58f3737b9791b6fdef1864
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-03-18 20:06:02 +0530

    Adding changes and configurations to embed the documentation of downstream services

--- src/main/java/com/zuul/gateway/proxy/server/NetflixZuulApiGatewayProxyServerApplication.java
+++ src/main/java/com/zuul/gateway/proxy/server/NetflixZuulApiGatewayProxyServerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import com.zuul.gateway.proxy.server.filters.ErrorFilter;
 import com.zuul.gateway.proxy.server.filters.PostFilter;
 import com.zuul.gateway.proxy.server.filters.PreFilter;
@@ -14,27 +17,33 @@ import com.zuul.gateway.proxy.server.filters.RouteFilter;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableSwagger2
 public class NetflixZuulApiGatewayProxyServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixZuulApiGatewayProxyServerApplication.class, args);
 	}
-	
-	@Bean
-    public PreFilter preFilter() {
-        return new PreFilter();
-    }
-    @Bean
-    public PostFilter postFilter() {
-        return new PostFilter();
-    }
-    @Bean
-    public ErrorFilter errorFilter() {
-        return new ErrorFilter();
-    }
-    @Bean
-    public RouteFilter routeFilter() {
-        return new RouteFilter();
-    }
 
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
+				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+	}	
 }
